d78bfa99b74c2ba378f1d834904cb37b0f292c13
jhbarnum <jhbarnum@gmail.com>
2026-02-05 12:54:26 -0500

    pages

---
+++ ShoppingList.Automation/pages/shoppingList.ts
@@ -0,0 +1,23 @@
+// LoginPage.ts
+import { type Page, type Locator } from '@playwright/test';
+
+export class LoginPage {
+  readonly page: Page;
+  readonly header: Locator;
+  readonly textInput: Locator;
+  readonly addButton: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    // Define locators for elements
+    this.header = page.locator('#username');
+    this.textInput = page.locator('#password');
+    this.addButton = page.locator('button[type="submit"]');
+  }
+
+  // Method to navigate to the list page
+  async goto() {
+    await this.page.goto('https://exampletestapp-hvd2cbazc8gmbsfk.centralus-01.azurewebsites.net/');
+  }
+
+}
