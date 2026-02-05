248c5c7f25b2aa141c9d544a0f1da38b1f238825
jhbarnum <jhbarnum@gmail.com>
2026-02-05 17:49:42 -0500

    test

--- ShoppingList.Automation/playwright.config.ts
+++ ShoppingList.Automation/playwright.config.ts
@@ -1,13 +1,17 @@
 import { defineConfig, devices } from '@playwright/test';
 
+const baseURL = (process.env.PLAYWRIGHT_BASE_URL ??
+  'http://localhost:5173') as string;
+
 export default defineConfig({
   testDir: './tests',
   timeout: 30 * 1000,
   expect: { timeout: 5000 },
   fullyParallel: true,
   reporter: [['list'], ['html', { open: 'never' }]],
+  
   use: {
-   baseURL: 'http://localhost:5173',
+   baseURL: baseURL,
     headless: true,
     viewport: { width: 1280, height: 800 },
     actionTimeout: 0,
