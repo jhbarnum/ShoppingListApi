7fbc65fef2e2e4854a4d5a6176ab5ee58904fca0
jhbarnum <jhbarnum@gmail.com>
2026-02-06 14:21:00 -0500

    rename

--- ShoppingList.Automation/tests/home.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '../fixtures/fixtures';
-
-test('goes to Azure', async ({ shoppingList }) => {
-  await shoppingList.goto('https://exampletestapp-hvd2cbazc8gmbsfk.centralus-01.azurewebsites.net');
-  await expect(shoppingList.header).toBeVisible();
-});
